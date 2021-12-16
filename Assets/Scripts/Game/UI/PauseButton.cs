ce5e71217d12795d7432bb63422e1e7857a3c8c4
Agent5S <19892761+Agent5S@users.noreply.github.com>
2021-12-15 23:51:34 -0600

    Update HUD sprites

--- Assets/Scripts/Game/UI/PauseButton.cs
+++ Assets/Scripts/Game/UI/PauseButton.cs
@@ -1,11 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PauseButton : MonoBehaviour
 {
+    public Sprite playSrite;
+    public Sprite pauseSprite;
+
+    public Image image;
+
     public void Pause()
     {
-        Time.timeScale = Time.timeScale > 0 ? 0 : 1;
+        var pause = Time.timeScale > 0;
+        this.image.sprite = pause ? playSrite : pauseSprite;
+        Time.timeScale = pause ? 0 : 1;
     }
 }
